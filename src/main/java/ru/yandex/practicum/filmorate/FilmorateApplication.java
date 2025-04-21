79f611fe42166b0dd287f41c536884cda7773679
Jalecti <john.n1998@yandex.ru>
2025-04-21 14:56:33 +0300

    testt

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FilmorateApplication {
     public static void main(String[] args) {
-        //asdas
+        //asdasasdasdasd
         SpringApplication.run(FilmorateApplication.class, args);
     }
 }
