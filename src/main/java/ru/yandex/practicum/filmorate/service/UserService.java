0e51f2e4e2bcf2481a844aa522c63a576907a618
Jalecti <john.n1998@yandex.ru>
2025-04-22 19:59:58 +0300

    Checkstyle fix

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.service;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dal.repositories.FriendshipRepository;
 import ru.yandex.practicum.filmorate.dal.repositories.UserEventRepository;
