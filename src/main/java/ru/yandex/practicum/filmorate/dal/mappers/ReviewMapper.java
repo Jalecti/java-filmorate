a73f9241d1f176ec2bce2ea7e264eed23567eade
n5n1m2 <n5n1m2@mail.ru>
2025-04-22 07:44:14 +0300

    Add Reviews

---
+++ src/main/java/ru/yandex/practicum/filmorate/dal/mappers/ReviewMapper.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum.filmorate.dal.mappers;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Review;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class ReviewMapper implements RowMapper<Review> {
+    @Override
+    public Review mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Review(rs.getInt("review_id"),
+                rs.getString("content"),
+                rs.getBoolean("is_positive"),
+                rs.getInt("user_id"),
+                rs.getInt("film_id"),
+                rs.getInt("useful"));
+    }
+}
