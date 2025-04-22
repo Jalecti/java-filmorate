a82c12d95b72f60cdf87942909e7ce0efea0a568
Jalecti <john.n1998@yandex.ru>
2025-04-22 19:57:04 +0300

    Checkstyle fix

--- src/main/java/ru/yandex/practicum/filmorate/model/UserEvent.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/UserEvent.java
@@ -1,11 +1,8 @@
 package ru.yandex.practicum.filmorate.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
-import java.time.Instant;
 
 @Data
 @EqualsAndHashCode(of = {"eventId"})
