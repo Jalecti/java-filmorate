219563a53e750f905283116c6192cb120692a814
Isakov Leonid <isakov.leonid@gmail.com>
2025-04-23 17:56:03 +0300

    Права codestyle

--- src/main/java/ru/yandex/practicum/filmorate/dal/mappers/ReviewMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/mappers/ReviewMapper.java
@@ -11,11 +11,11 @@ import java.sql.SQLException;
 public class ReviewMapper implements RowMapper<Review> {
     @Override
     public Review mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return new Review(rs.getInt("review_id"),
+        return new Review(rs.getLong("review_id"),
                 rs.getString("content"),
                 rs.getBoolean("is_positive"),
-                rs.getInt("user_id"),
-                rs.getInt("film_id"),
+                rs.getLong("user_id"),
+                rs.getLong("film_id"),
                 rs.getInt("useful"));
     }
 }
